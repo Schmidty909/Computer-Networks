7fb3ade5e4ddd9d5bd765908bb709b7a38902254
Schmidty909 <matthew.schmidty@aol.com>
2022-04-08 00:55:45 -0400

    Added some more stuff, added Network.py to branch. Server starts and threads are created.

--- client.py
+++ client.py
@@ -1,28 +1,32 @@
 import socket
 import pygame 
 
-width = 500
-height = 500
+width = 1000
+height = 800
 
-window = pygame.display.set_mode(width, height)
+window = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Captain Admiral")
 
 clientNumber = 0
 
 
-class Submarine():
-    def __init__ (self, x, y, width, height, color):
+class Player():
+    def __init__(self, x, y, width, height, color):
         self.x = x
         self.y = y
         self.width = width
         self.height = width
         self.color = color
-        self.rect(x, y, width, height)
+        self.rect = (x,y,width,height)
         self.vel = 5
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, self.rect)
+
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.rect)
+
+
     def move(self):
-        keys = pygame.keys.get_pressed()
+        keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
             self.x -= self.vel
@@ -31,26 +35,33 @@ class Submarine():
             self.x += self.vel
 
         if keys[pygame.K_UP]:
-            self.y += self.vel
+            self.y -= self.vel
 
         if keys[pygame.K_DOWN]:
-            self.y -= self.vel
+            self.y += self.vel
 
         self.rect = (self.x, self.y, self.width, self.height)
 
-def redrawWindow(player, win):
-    win.fill(255,255,255)
-    player.draw(win)
-    pygam.display.update()
+
+def redrawWindow(p1, window):
+    window.fill((255,255,255))
+    p1.draw(window)
+    pygame.display.update()
 
 
 def main():
     run = True
-    p1 = Submarine(50, 50, 100, 100,(255,0,0))
+    p = Player(450, 450, 100, 100, (255,0,0))
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(30)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        p1.move
-        redrawWindow(p1, win)
+        p.move()
+        redrawWindow(p, window)
+
+
+main()
