1156716981b8b7e5a7de218d522e19d0b36eed0b
Juan <jsoto_columbus@hotmail.com>
2022-04-19 06:52:01 -0400

    Board is now more dynamic. The menu boxes and text aren't dynamic so, that needs adjustment with time

--- clientDictionary.py
+++ clientDictionary.py
@@ -12,14 +12,15 @@ import pygame.mixer_music
 
 pygame.init()
 
-win_width = 1050
-win_height = 1050
+win_width = 1100
+win_height = 1100
 
 playerX = 0
 playerY = 0
 Key = "a"
 KeyPosition = 0
 move = True
+game_dictionary= {}
 
 white = [255, 255, 255]
 red = (255, 0, 0)
@@ -30,14 +31,14 @@ black = (0, 0, 0)
 
 
 window = pygame.display.set_mode((win_width, win_height))
-pygame.display.set_caption("Captain Admiral")
 
-#DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 font = pygame.font.Font('cour.ttf', 40)
 
 clientNumber = 0
-
+# width can't be greater then 26
+# Length can't be greater then 9
 grid_width = 10
 grid_length = 10
 
@@ -81,7 +82,6 @@ class game_grid():
         self.initalcolor = ocean_blue
         self.rect = pygame.draw.rect(window, self.color, [self.x, self.y , 100, 100])
         self.island = False
-        self.has_player = False
     def hover(self):
         global playerX
         global playerY
@@ -173,16 +173,20 @@ def build_game_grid():
 def print_game_grid():
     ## Loops to make Row and Column headers
     counter = 85
-    for x in range(65, 75, 1):
-        top_row_letters = font.render(chr(x), True, grey)
+    top_character = 65
+    for x in range(grid_width):
+        top_row_letters = font.render(chr(top_character), True, grey)
         window.blit(top_row_letters, (counter, 0))
         counter += 100
+        top_character += 1
 
     counter = 80
-    for y in range(48, 58, 1):
-        side_row_numbers = font.render(chr(y), True, grey)
+    side_numbers = 48
+    for y in range(grid_length ):
+        side_row_numbers = font.render(chr(side_numbers), True, grey)
         window.blit(side_row_numbers, (5, counter))
         counter += 100
+        side_numbers += 1
     ## Loops to make square pieces
     for key in game_dictionary.keys():
         for count,index in enumerate(game_dictionary[key]):
@@ -269,18 +273,38 @@ def ValidateMovementY(y,key,keylocation,flag):
                 p.location = p.location + 1
 
 
+def generate_dictionary():
+    list = []
+    character = 97
 
-game_dictionary = {"a": [game_grid(50,50,"a",0),game_grid(50,150,"a",1),game_grid(50,250,"a",2),game_grid(50,350,"a",3),game_grid(50,450,"a",4),game_grid(50,550,"a",5),game_grid(50,650,"a",6),game_grid(50,750,"a",7),game_grid(50,850,"a",8),game_grid(50,950,"a",9)],
-                   "b": [game_grid(150,50,"b",0),game_grid(150,150,"b",1),game_grid(150,250,"b",2),game_grid(150,350,"b",3),game_grid(150,450,"b",4),game_grid(150,550,"b",5),game_grid(150,650,"b",6),game_grid(150,750,"b",7),game_grid(150,850,"b",8),game_grid(150,950,"b",9)],
-                   "c": [game_grid(250,50,"c",0),game_grid(250,150,"c",1),game_grid(250,250,"c",2),game_grid(250,350,"c",3),game_grid(250,450,"c",4),game_grid(250,550,"c",5),game_grid(250,650,"c",6),game_grid(250,750,"c",7),game_grid(250,850,"c",8),game_grid(250,950,"c",9)],
-                   "d": [game_grid(350,50,"d",0),game_grid(350,150,"d",1),game_grid(350,250,"d",2),game_grid(350,350,"d",3),game_grid(350,450,"d",4),game_grid(350,550,"d",5),game_grid(350,650,"d",6),game_grid(350,750,"d",7),game_grid(350,850,"d",8),game_grid(350,950,"d",9)],
-                   "e": [game_grid(450,50,"e",0),game_grid(450,150,"e",1),game_grid(450,250,"e",2),game_grid(450,350,"e",3),game_grid(450,450,"e",4),game_grid(450,550,"e",5),game_grid(450,650,"e",6),game_grid(450,750,"e",7),game_grid(450,850,"e",8),game_grid(450,950,"e",9)],
-                   "f": [game_grid(550,50,"f",0),game_grid(550,150,"f",1),game_grid(550,250,"f",2),game_grid(550,350,"f",3),game_grid(550,450,"f",4),game_grid(550,550,"f",5),game_grid(550,650,"f",6),game_grid(550,750,"f",7),game_grid(550,850,"f",8),game_grid(550,950,"f",9)],
-                   "g": [game_grid(650,50,"g",0),game_grid(650,150,"g",1),game_grid(650,250,"g",2),game_grid(650,350,"g",3),game_grid(650,450,"g",4),game_grid(650,550,"g",5),game_grid(650,650,"g",6),game_grid(650,750,"g",7),game_grid(650,850,"g",8),game_grid(650,950,"g",9)],
-                   "h": [game_grid(750,50,"h",0),game_grid(750,150,"h",1),game_grid(750,250,"h",2),game_grid(750,350,"h",3),game_grid(750,450,"h",4),game_grid(750,550,"h",5),game_grid(750,650,"h",6),game_grid(750,750,"h",7),game_grid(750,850,"h",8),game_grid(750,950,"h",9)],
-                   "i": [game_grid(850,50,"i",0),game_grid(850,150,"i",1),game_grid(850,250,"i",2),game_grid(850,350,"i",3),game_grid(850,450,"i",4),game_grid(850,550,"i",5),game_grid(850,650,"i",6),game_grid(850,750,"i",7),game_grid(850,850,"i",8),game_grid(850,950,"i",9)],
-                   "j": [game_grid(950,50,"j",0),game_grid(950,150,"j",1),game_grid(950,250,"j",2),game_grid(950,350,"j",3),game_grid(950,450,"j",4),game_grid(950,550,"j",5),game_grid(950,650,"j",6),game_grid(950,750,"j",7),game_grid(950,850,"j",8),game_grid(950,950,"j",9)]
-}
+    for x in range(grid_width + 1):
+        game_dictionary[chr(character)] = list
+        print(game_dictionary)
+        list = []
+        position = 0
+        character = 97
+        print(x)
+        character += x
+        print(character)
+        for y in range(grid_length ):
+            list.append(game_grid(x* 100 + 50, y * 100 + 50, chr(character), position))
+            position += 1
+
+
+
+
+
+# game_dictionary = {"a": [game_grid(50,50,"a",0),game_grid(50,150,"a",1),game_grid(50,250,"a",2),game_grid(50,350,"a",3),game_grid(50,450,"a",4),game_grid(50,550,"a",5),game_grid(50,650,"a",6),game_grid(50,750,"a",7),game_grid(50,850,"a",8),game_grid(50,950,"a",9)],
+#                    "b": [game_grid(150,50,"b",0),game_grid(150,150,"b",1),game_grid(150,250,"b",2),game_grid(150,350,"b",3),game_grid(150,450,"b",4),game_grid(150,550,"b",5),game_grid(150,650,"b",6),game_grid(150,750,"b",7),game_grid(150,850,"b",8),game_grid(150,950,"b",9)],
+#                    "c": [game_grid(250,50,"c",0),game_grid(250,150,"c",1),game_grid(250,250,"c",2),game_grid(250,350,"c",3),game_grid(250,450,"c",4),game_grid(250,550,"c",5),game_grid(250,650,"c",6),game_grid(250,750,"c",7),game_grid(250,850,"c",8),game_grid(250,950,"c",9)],
+#                    "d": [game_grid(350,50,"d",0),game_grid(350,150,"d",1),game_grid(350,250,"d",2),game_grid(350,350,"d",3),game_grid(350,450,"d",4),game_grid(350,550,"d",5),game_grid(350,650,"d",6),game_grid(350,750,"d",7),game_grid(350,850,"d",8),game_grid(350,950,"d",9)],
+#                    "e": [game_grid(450,50,"e",0),game_grid(450,150,"e",1),game_grid(450,250,"e",2),game_grid(450,350,"e",3),game_grid(450,450,"e",4),game_grid(450,550,"e",5),game_grid(450,650,"e",6),game_grid(450,750,"e",7),game_grid(450,850,"e",8),game_grid(450,950,"e",9)],
+#                    "f": [game_grid(550,50,"f",0),game_grid(550,150,"f",1),game_grid(550,250,"f",2),game_grid(550,350,"f",3),game_grid(550,450,"f",4),game_grid(550,550,"f",5),game_grid(550,650,"f",6),game_grid(550,750,"f",7),game_grid(550,850,"f",8),game_grid(550,950,"f",9)],
+#                    "g": [game_grid(650,50,"g",0),game_grid(650,150,"g",1),game_grid(650,250,"g",2),game_grid(650,350,"g",3),game_grid(650,450,"g",4),game_grid(650,550,"g",5),game_grid(650,650,"g",6),game_grid(650,750,"g",7),game_grid(650,850,"g",8),game_grid(650,950,"g",9)],
+#                    "h": [game_grid(750,50,"h",0),game_grid(750,150,"h",1),game_grid(750,250,"h",2),game_grid(750,350,"h",3),game_grid(750,450,"h",4),game_grid(750,550,"h",5),game_grid(750,650,"h",6),game_grid(750,750,"h",7),game_grid(750,850,"h",8),game_grid(750,950,"h",9)],
+#                    "i": [game_grid(850,50,"i",0),game_grid(850,150,"i",1),game_grid(850,250,"i",2),game_grid(850,350,"i",3),game_grid(850,450,"i",4),game_grid(850,550,"i",5),game_grid(850,650,"i",6),game_grid(850,750,"i",7),game_grid(850,850,"i",8),game_grid(850,950,"i",9)],
+#                    "j": [game_grid(950,50,"j",0),game_grid(950,150,"j",1),game_grid(950,250,"j",2),game_grid(950,350,"j",3),game_grid(950,450,"j",4),game_grid(950,550,"j",5),game_grid(950,650,"j",6),game_grid(950,750,"j",7),game_grid(950,850,"j",8),game_grid(950,950,"j",9)]
+# }
 
 def main_menu():
 
@@ -292,15 +316,15 @@ def main_menu():
 
     text = font.render("Admiral Acknowledge", True, white)
     textRect = text.get_rect()
-    textRect.center = (win_width // 2, win_height // 2 - 150)
+    textRect.center = (win_width // 2, win_height // 2 - 180)
 
     start = font.render("Start", True, white)
     startRect = start.get_rect()
-    startRect.center = (win_width // 2 , win_height // 2 - 75)
+    startRect.center = (win_width // 2 -25 , win_height // 2 - 100)
 
     Quit = font.render("Quit", True, white)
     QuitRect = Quit.get_rect()
-    QuitRect.center = (win_width // 2 , win_height // 2 )
+    QuitRect.center = (win_width // 2 -25, win_height // 2-25 )
 
 
 
@@ -320,6 +344,7 @@ def fade(width, height):
         pygame.time.delay(5)
 
 def main():
+    generate_dictionary()
     main = True
     run = True
     Selection = True
@@ -385,8 +410,7 @@ def main():
     # # Start playing the song
     pygame.mixer.music.play()
 
-
-
+    pygame.display.set_caption("Captain Admiral")
 
     build_game_grid()
 
