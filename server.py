6a47a4aa678906eb71eeaf5df28faf0093b85cf8
Juan <jsoto_columbus@hotmail.com>
2022-04-21 20:23:26 -0400

    Working server and Network code

--- server.py
+++ server.py
@@ -1,47 +1,138 @@
-import random
+import pygame
+import json
 import socket
 from _thread import *
+import sys
+import jsonpickle
+game_dictionary = {}
+grid_width = 10
+grid_length = 10
 
-server = "192.168.0.4"
-port = 55555
+white = [255, 255, 255]
+red = (255, 0, 0)
+green = (0, 255, 0)
+ocean_blue = (0, 130, 150)
+grey = (128, 128, 128)
+black = (0, 0, 0)
+
+server= "192.168.1.133"
+port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    s.bind((server, port))
+    s.bind((server,port))
 except socket.error as e:
     str(e)
 
 s.listen(2)
-print("Waiting for connection, Server Started")
+print("Waiting for connection, server started")
 
+def generate_dictionary():
+    list = []
+    character = 97
+
+    for x in range(grid_width + 1):
+        game_dictionary[chr(character)] = list
+        list = []
+        position = 0
+        character = 97
+        character += x
+        for y in range(grid_length ):
+            list.append(game_grid(x* 100 + 50, y * 100 + 50, chr(character), position))
+            position += 1
+
+
+class game_grid():
+
+    def __init__(self,x,y,key,location):
+        self.x = x
+        self.y = y
+        self.key = key
+        self.location = location
+        self.color = ocean_blue
+        self.initalcolor = ocean_blue
+        self.island = False
 
 def threaded_client(conn):
+    print(game_dictionary)
+    jsonStr = jsonpickle.encode(game_dictionary)
+
+    conn.send(str.encode(jsonStr))
     reply = ""
     while True:
         try:
             data = conn.recv(2048)
             reply = data.decode("utf-8")
-
             if not data:
                 print("Disconnected")
                 break
             else:
-                print("Received:", reply)
-                print("Sending: ", reply)
-
+                print("Recevied: ", reply)
+                print("Sending: ",reply)
             conn.sendall(str.encode(reply))
         except:
             break
-
     print("Lost Connection")
     conn.close()
 
+
+generate_dictionary()
 while True:
     conn, addr = s.accept()
-    print("Connected to:", addr)
+    print("Connnected to:",addr)
 
     start_new_thread(threaded_client, (conn,))
+
+
+
+
+
+
+# import random
+# import socket
+# from _thread import *
+#
+# server = "192.168.0.4"
+# port = 55555
+#
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+# try:
+#     s.bind((server, port))
+# except socket.error as e:
+#     str(e)
+#
+# s.listen(2)
+# print("Waiting for connection, Server Started")
+#
+#
+# def threaded_client(conn):
+#     reply = ""
+#     while True:
+#         try:
+#             data = conn.recv(2048)
+#             reply = data.decode("utf-8")
+#
+#             if not data:
+#                 print("Disconnected")
+#                 break
+#             else:
+#                 print("Received:", reply)
+#                 print("Sending: ", reply)
+#
+#             conn.sendall(str.encode(reply))
+#         except:
+#             break
+#
+#     print("Lost Connection")
+#     conn.close()
+#
+# while True:
+#     conn, addr = s.accept()
+#     print("Connected to:", addr)
+#
+#     start_new_thread(threaded_client, (conn,))
 
 # Server side code for Admiral Acknownledgement
 # To do:
