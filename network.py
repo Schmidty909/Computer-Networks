358314a0cb940ffdfc2ab3517dd0215893c6296b
Schmidty909 <matthew.schmidty@aol.com>
2022-04-23 19:12:18 -0400

    Renamed clientDic to client2

--- network.py
+++ network.py
@@ -5,26 +5,24 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.server = "75.187.185.215"
+        self.server = "192.168.0.8"
         self.port = 5555
         self.addr = (self.server, self.port)
         self.id = self.connect()
-        print(self.id)
+
     def Board(self):
         return self.id
+
     def connect(self):
         try:
             self.client.connect(self.addr)
             return pickle.loads(self.client.recv(20000))
-
         except:
             pass
-    def send(self,data):
+
+    def send(self, data):
         try:
             self.client.send(pickle.dumps(data))
-            return pickle.loads(self.client.recv(20000))
+            return self.client.recv(20000).decode("UTF-8")
         except socket.error as e:
             print(e)
-
-
-
